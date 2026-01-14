249fc0b6c2555f79f1162ad0c3737e3bcceffae7
ddubost2708 <ddubost2708@icloud.com>
2026-01-14 11:35:54 +0100

    Add deployment project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ddubost2708.github.io",
+  base: "portfolio",
+  trailingSlash: "always",
   integrations: [tailwind()],
 });
